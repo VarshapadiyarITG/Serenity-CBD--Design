0c8875dbcfae79deb18d6396dc679dd563f6996f
vasrha <varshapadiyar@itgeeks.com>
2025-10-15 20:11:50 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
- var swiper = new Swiper(".mySwiper", {
-      pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-      },
-    });
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
+
+
+var swiper = new Swiper(".swiper-component", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagi: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
