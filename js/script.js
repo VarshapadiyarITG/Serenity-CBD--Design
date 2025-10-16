a7a0a85125bf36ec59737b9d098203c752308017
vasrha <varshapadiyar@itgeeks.com>
2025-10-16 20:21:43 +0530

    change

--- js/script.js
+++ js/script.js
@@ -27,4 +27,32 @@ var swiper = new Swiper(".swiper-component", {
       spaceBetween: 30,
     },
   },
+
+});
+
+var swiper = new Swiper(".swiper-product", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagi: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-second",
+    prevEl: ".swiper-button-previous",
+  },
 });
